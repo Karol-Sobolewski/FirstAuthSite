627eb3551e307bdcc198728288d7d0e7f9101b5e
Karol <karolsobolewski92@gmail.com>
2020-10-05 13:14:41 +0200

    Add avatar for user panel

---
+++ routes/user.routes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const router = express.Router();
+
+const isLogged = (req, res, next) => {
+  if (!req.user) {
+    res.redirect('no-permission');
+  } else {
+    next();
+  }
+};
+
+router.get('/logged', isLogged, (req, res) => {
+  console.log(req.user);
+  res.render('logged', {
+    username: req.user._json.given_name,
+    picture: req.user._json.picture,
+  });
+});
+
+router.get('/no-permission', (req, res) => {
+  res.render('noPermission');
+});
+
+router.get('/profile', isLogged, (req, res) => {
+  res.render('profile');
+});
+
+router.get('/profile/settings', isLogged, (req, res) => {
+  res.render('settings');
+});
+
+module.exports = router;
